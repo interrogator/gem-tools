569c3d4103390990653d2b9ef3cd4318b5a597dd
daniel <mcdonaldd@unimelb.edu.au>
2016-07-14 15:17:05 +0200

    i'd propose a tiny gui that can be called when not in ipython :)

---
+++ gem_tools/gui.py
@@ -0,0 +1,24 @@
+"""gem-tools simple gui"""
+
+from tkinter import Tk, Label, Button
+
+class GemGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("gem-tools")
+
+        self.label = Label(master, text="Simple gem-interface!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        print("Testing!")
+
+root = Tk()
+my_gui = GemGUI(root)
+root.mainloop()
