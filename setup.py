478e7bbf01b9630e28b299425501e83f7422978d
daniel <mcdonaldd@unimelb.edu.au>
2016-07-14 14:28:19 +0200

    adding distribution data

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+setup(name='gem-tools',
+      version='0.0.1',
+      description='Tools for working with multimodal corpora annotated using the Genre and Multimodality model',
+      url='https://github.com/thiippal/gem-tools',
+      author='Tuomo Hiippala',
+      packages=['gem-tools'],
+      package_dir={'gem-tools': 'gem-tools'},
+      package_data={'gem-tools': ['*.pkl', 'model/*.pkl', '*.ipynb', '*.ipynb']},
+      author_email='tuomo.hiippala@helsinki.fi',
+      license='MIT',
+      keywords=['multimodality', 'imaging', 'ocr'],
+      install_requires=["graphviz>=0.4.10"])
