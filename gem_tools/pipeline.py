d20c21a210a548230c41f7556bfe0e00a7625a2a
daniel <mcdonaldd@unimelb.edu.au>
2016-07-14 15:03:29 +0200

    fix importerror

--- gem_tools/pipeline.py
+++ gem_tools/pipeline.py
@@ -1,22 +1,22 @@
 """A simple function that runs the entire pipeline"""
 
 def process(filename, **kwargs):
-    from gem.generator import (classify,
-                               describe,
-                               detect_roi,
-                               draw_roi,
-                               extract_bu,
-                               false_positives,
-                               generate_annotation,
-                               generate_graphics,
-                               generate_text,
-                               preprocess,
-                               project,
-                               redraw,
-                               load_model,
-                               sort_contours,
-                               tokenize,
-                               vlog)
+    from gem_tools.generator import (classify,
+                                     describe,
+                                     detect_roi,
+                                     draw_roi,
+                                     extract_bu,
+                                     false_positives,
+                                     generate_annotation,
+                                     generate_graphics,
+                                     generate_text,
+                                     preprocess,
+                                     project,
+                                     redraw,
+                                     load_model,
+                                     sort_contours,
+                                     tokenize,
+                                     vlog)
     # do something here :)
 
     return
